89660c18fbf87fdcab7f40f1b9d672296ae29cec
MarianaBejarano <mariana.bejarano@bue.edu.ar>
2017-06-27 21:21:28 -0300

    Agregado datos geograficos

---
+++ src/factura/Partido.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factura;
+
+/**
+ *
+ * @author PC22
+ */
+public class Partido {
+ 
+    private String Nombre;
+    private String Alias;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getAlias() {
+        return Alias;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public void setAlias(String Alias) {
+        this.Alias = Alias;
+    }
+    
+}
